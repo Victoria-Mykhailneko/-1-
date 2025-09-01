3e44c82ffba6e1bc296a1a4ff9deceac1c7711b0
user <user@WIN-RVFHG9FKSQG>
2025-09-01 09:22:26 +0300

    Add project files.

---
+++ ПЗ1/ПЗ1.cpp
@@ -0,0 +1,60 @@
+﻿// ПЗ1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    system("color F1");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // Прізвище
+    string last_name;
+    cout << "Введіть прізвище: ";
+    getline(cin, last_name);
+
+    // Ім'я
+    string first_name;
+    cout << "Введіть ім'я: ";
+    getline(cin, first_name);
+
+    // Вік
+    int age;
+    cout << "Введіть вік: ";
+    cin >> age;
+
+    // Курс
+    int course;
+    cout << "Введіть курс: ";
+    cin >> course;
+    cin.ignore();
+
+    // Місто проживання
+    string city;
+    cout << "Введіть місто проживання: ";
+    getline(cin, city);
+
+    // Хобі
+    string hobby;
+    cout << "Введіть ваше хобі: ";
+    getline(cin, hobby);
+
+    // Очищення буфера після введення числа
+    cin.ignore();
+
+    // Вивід введених даних
+    cout << "\nВаші дані:" << endl;
+    cout << "Прізвище: " << last_name << endl;
+    cout << "Ім'я: " << first_name << endl;
+    cout << "Вік: " << age << endl;
+    cout << "Курс: " << course << endl;
+    cout << "Місто проживання: " << city << endl;
+    cout << "Хобі: " << hobby << endl;
+
+    return 0;
+}
+
+
